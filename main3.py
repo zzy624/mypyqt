90fb28979a31922bc8dd61353dffdbd32b80b3bb
zzy <zhangzhiyuan624@163.com>
2018-07-03 00:53:58 +0800

    up

--- main3.py
+++ main3.py
@@ -1,63 +1,11 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+from ethereum.utils import *
+import bitcoin
+import os
+key = os.urandom(32)
+private_key = encode_hex(key)
+public_key = encode_hex(bitcoin.privkey_to_pubkey(key))
+address = '0x' + encode_hex(privtoaddr(key))
 
-"""
-PyQt5 教程
-
-这个例子显示了一个进度条控件。
-
-作者：我的世界你曾经来过
-博客：http://blog.csdn.net/weiaitaowang
-最后编辑：2016年8月3日
-"""
-
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QProgressBar, QPushButton
-from PyQt5.QtCore import QBasicTimer
-
-class Example(QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-
-        self.pbar = QProgressBar(self)
-        self.pbar.setGeometry(30, 40, 200, 25)
-
-        self.btn = QPushButton('开始', self)
-        self.btn.move(40, 80)
-        self.btn.clicked.connect(self.doAction)
-
-        self.timer = QBasicTimer()
-        self.step = 0
-
-        self.setGeometry(300, 300, 280, 170)
-        self.setWindowTitle('进度条')
-        self.show()
-
-    def timerEvent(self, e):
-
-        if self.step >= 100:
-            self.timer.stop()
-            self.btn.setText('完成')
-            return
-        self.step = self.step+1
-        self.pbar.setValue(self.step)
-
-    def doAction(self, value):
-
-        if self.timer.isActive():
-            self.timer.stop()
-            self.btn.setText('开始')
-        else:
-            self.timer.start(1000, self)
-            self.btn.setText('停止')
-
-if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+print(private_key)
+print(public_key)
+print(address)
